e565953f60650a9e437f9742bef5b46048ca16af
Jianlong Chen <jianlong99@gmail.com>
2014-11-26 14:23:20 +0800

    Add reverse gist

---
+++ reverse.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8:et
+"""Reverse a list/str or anything support [] operator
+"""
+__author__ = ["Jianlong Chen <jianlong99@gmail.com>"]
+__date__ = "2013-07-17"
+
+ def reverse(t):
+     """Reverse a list/str or anything support [] operator"""
+     return t[::-1]
